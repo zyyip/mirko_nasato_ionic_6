14b81289cb8b7e93ac31b74879c4a0742e13b4f1
zyyip <64174391+zyyip@users.noreply.github.com>
2022-02-05 23:07:40 +0800

    Finished Part 88. Allowing users to input a picture in the entry page

--- src/pages/AddEntryPage.tsx
+++ src/pages/AddEntryPage.tsx
@@ -23,8 +23,9 @@ import { firestore } from '../firebase';
 const AddEntryPage: React.FC = () => {
   const { userId } = useAuth();
   const [ date, setDate ] = useState('');
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  const [ title, setTitle ] = useState('');
+  const [ description, setDescription ] = useState('');
+  const [ pictureUrl, setPictureUrl ] = useState('');
   const history = useHistory();
 
   const handleSave = async () =>{
@@ -35,6 +36,16 @@ const AddEntryPage: React.FC = () => {
     console.log('saved: ', entryRef.id);
     history.goBack();
   }
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+    if(event.target.files.length>0){
+      const file = event.target.files.item(0);
+      const pictureUrl = URL.createObjectURL(file);
+      setPictureUrl(pictureUrl);
+      console.log('create URL:', pictureUrl);
+    }
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -54,6 +65,13 @@ const AddEntryPage: React.FC = () => {
           <IonItem>
             <IonLabel position="stacked">Title</IonLabel>
             <IonInput value={title} onIonChange={(event) => setTitle(event.detail.value)}/>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Picture</IonLabel>
+            <input type="file" accept="image/*" 
+              onChange={handleFileChange}
+            />
+            <img src={pictureUrl} alt=""/>
           </IonItem>
           <IonItem>
             <IonLabel position="stacked">Description</IonLabel>
